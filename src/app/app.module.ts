515b9cbd75ccc9f090969184bf594377d0d8294d
Sorin Vestemean <vestemeansorin91@gmail.com>
2021-02-26 16:54:57 +0200

    angular material added + refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [AppRoutes, BrowserModule, SharedModule],
+  imports: [AppRoutes, BrowserModule, SharedModule, BrowserAnimationsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,17 @@
-import { ModalComponent } from './components/modal/modal.component';
 import { FootballApiInterceptor } from './services/football-api.interceptor';
 import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NavbarComponent } from './layout/navbar/navbar.component';
 
-const COMPONENTS = [NavbarComponent, ModalComponent];
+import {MatDialogModule} from '@angular/material/dialog';
+
+
+const ANGULAR_MATERIAL_MODULES = [MatDialogModule]
+
 
 @NgModule({
-  declarations: [COMPONENTS],
-  imports: [CommonModule, HttpClientModule, RouterModule],
-  providers:[, { provide: HTTP_INTERCEPTORS, useClass: FootballApiInterceptor, multi: true }],
-  exports: [COMPONENTS],
+  imports: [CommonModule, HttpClientModule, RouterModule, ANGULAR_MATERIAL_MODULES],
+  providers:[{ provide: HTTP_INTERCEPTORS, useClass: FootballApiInterceptor, multi: true }],
 })
 export class SharedModule {}
